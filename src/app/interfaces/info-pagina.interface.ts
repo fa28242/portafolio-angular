25d6533912a4f3249f27961028ecc1b1c7eb763d
Francisco Alvarez <alvarezhiguita@gmail.com>
2020-10-02 14:23:05 -0500

    Fin de la selección 6

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,15 @@
+
+
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  pagoina_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tumblr?: string;
+  equipo_trabajo?: any[];
+
+}
---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+
+  equipo: any[];
+
+
+  constructor( private http: HttpClient) {
+
+    console.log('Servicio de Info Página LISTO!!!!');
+
+    this.cargarInfo();
+    this.cargarEquipo();
+
+
+  }
+
+  // tslint:disable-next-line: typedef
+  private cargarInfo(){
+    // Leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+        .subscribe( (resp: InfoPagina) => {
+          this.cargada = true;
+          this.info = resp;
+        });
+  }
+
+  // tslint:disable-next-line: typedef
+  private cargarEquipo() {
+      // Leer el archivo JSON
+    this.http.get('https://angular-hmtl.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
+    });
+  }
+}
